3243fea9e7349f873a0f27cb788af26946a1630d
ShrutiPatankar9 <87245432+ShrutiPatankar9@users.noreply.github.com>
2021-07-10 22:06:58 +0200

    Add files via upload

---
+++ BLE_sensor.ino
@@ -0,0 +1,37 @@
+#include <Arduino_HTS221.h>
+#include <Arduino_APDS9960.h>
+
+float temp_sensed;
+int gesture;
+
+void setup()
+{ 
+  Serial.begin(9600);               // initialize serial communication at 9600 bits per second:
+  Serial1.begin(9600);            // initialize UART with baud rate of 9600
+
+  HTS.begin();                                              //Activate Temperature Sensor
+
+  if (!APDS.begin()) {                                       //Gesture sensor checking
+    Serial1.println("Error initializing gesture sensor!");    
+  }
+}
+
+
+void loop() {
+
+//Gesture detection 
+for (int i = 0; i < 1000; i++)
+ {
+    APDS.setGestureSensitivity(90);
+    if(APDS.gestureAvailable()){ 
+    gesture = APDS.readGesture();
+    Serial1.println(gesture);
+    }
+    delay(10);
+ }
+
+//Temperature monitoring
+temp_sensed = HTS.readTemperature();
+Serial.println(temp_sensed);
+Serial1.println(temp_sensed);
+}
